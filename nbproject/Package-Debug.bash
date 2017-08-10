e2fc02366c1ee641ae4190856b96eed652140a82
Petr Stepanov <kisstherail@gmail.com>
2017-08-10 12:31:53 -0400

    Added Icons

--- nbproject/Package-Debug.bash
+++ nbproject/Package-Debug.bash
@@ -67,6 +67,11 @@ cd "${TOP}"
 makeDirectory "${NBTMPDIR}/sw-calculator/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
+cd "${TOP}"
+makeDirectory "${NBTMPDIR}/sw-calculator/bin"
+copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
+
+
 # Generate tar file
 cd "${TOP}"
 rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/sw-calculator.tar
